66297b2a52af0e23518169cd57ff64c73238c528
William Sheere IV <Sheerebilly@yahoo.com>
2024-10-31 13:46:07 -0400

    initial commit2

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+import inquirer from "inquirer";
+import fs from "fs";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    }
+];
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README file END
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer .prompt([
+        {
+            //type name message
+        }
+    ]
+
+    )
+}
 
 // Function call to initialize app
 init();
