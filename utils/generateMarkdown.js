f3babf333187dd641921cb83eb68ea3965465525
William Sheere IV <Sheerebilly@yahoo.com>
2024-11-03 12:39:22 -0500

    initial comitt

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {if (license) return (license);
+   if ((license))isEmpty('');
+		}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { if (license) return (license);
+   {
+			if ((license))isEmpty('');
+		}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+ function renderLicenseSection(license) { if ((license))isEmpty('');
+	}
+	return `## License\n\nThis project is licensed under the ${license} license.`;
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}`;
 }
-
 export default generateMarkdown;
